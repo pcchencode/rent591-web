c6ae87b4c182b4b2a2ddaae2d18e29b16ac9216f
pcchencode <zxp791228@gmail.com>
2021-12-21 11:33:27 +0800

    modeify index

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 # lhc = db.connect(host='127.0.0.1', user='root', password='')
 
 @app.route('/')
-def home_test():
+def index():
     return "SUCCESS"
 
 @app.route('/home-page')
@@ -120,7 +120,7 @@ def test_accordion():
 
 
 @app.route('/js-test')
-def index():
+def js_index():
     # js_test.html 會因為點擊觸發 javascript, 進而改變 html
     return render_template('js_test.html')
 
@@ -140,5 +140,5 @@ def card():
 
 if __name__ == '__main__':
     app.config['SECRET_KEY']='your key' #這是因為flask_wtf預設需要設置密碼，也是為了避免一開始所說的CSRF攻擊。
-    # app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
+    # app.run(host="0.0.0.0", port=5000)
