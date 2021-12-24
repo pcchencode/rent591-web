84d6bc56e3a5f4433cd58d2df3d1ad4f029cb7da
pcchencode <zxp791228@gmail.com>
2021-12-24 16:17:39 +0800

    update port

--- app.py
+++ app.py
@@ -144,4 +144,4 @@ def card():
 if __name__ == '__main__':
     app.config['SECRET_KEY']='your key' #這是因為flask_wtf預設需要設置密碼，也是為了避免一開始所說的CSRF攻擊。
     #app.run(debug=True)
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=8001)
