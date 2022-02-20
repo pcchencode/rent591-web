e4efaaed03122779cf3be2bdf6bcab1e089ad9f1
pcchencode <zxp791228@gmail.com>
2022-02-20 16:14:04 +0800

    update

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ app.config['SECRET_KEY']='your key' #這是因為flask_wtf預設需要設置密�
 
 @app.route('/')
 def index():
-    return "SUCCESS! test for K8S"
+    return "SUCCESS! test for K8S!"
 
 @app.route('/home-page')
 def home_page():
